9c63dcf7d4ffda3f95b25dece3cacf0d8dedb1a5
Samuel <sfoletto@gmail.com>
2025-06-04 21:26:24 -0300

    Initial commit

---
+++ dev/fundamentos/input.py
@@ -0,0 +1,9 @@
+name = input('Digite o nome do filme: \n')
+yearLaunch = int(input('Digite o ano de lançamento: \n'))
+noteMovie = float(input('Digite a nota do filme: \n'))
+
+print(name)
+print(yearLaunch)
+print(type(yearLaunch))
+print(noteMovie)
+print(type(noteMovie))
